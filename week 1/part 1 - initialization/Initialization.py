743a6b130e5c37edf80f10ed2deb873083b66146
fshamasna <firas.m.shamasneh@gmail.com>
2019-06-11 09:06:39 +0200

    zero and random initialization

---
+++ week 1/part 1 - initialization/Initialization.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 23:52:27 2019
+
+@author: Nahir
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# load image dataset: blue/red dots in circles
+train_X, train_Y, test_X, test_Y = load_dataset()
+
+def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True, initialization = "he"):
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    layers_dims = [X.shape[0],10,5,1]
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+    
+    for i in range(0,num_iterations):
+        a3, cache = forward_propagation(X, parameters)
+        cost = compute_loss(a3, Y)
+        grads = backward_propagation(X, Y, cache)
+        parameters = update_parameters(parameters, grads, learning_rate)
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+    
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+def initialize_parameters_zeros(layers_dims):
+    parameters = {}
+    L = len(layers_dims)
+    for l in range(1,L):
+        parameters ["W"+str(l)] = np.zeros(shape = (layers_dims[l],layers_dims[l-1]))
+        parameters ["b"+str(l)] = np.zeros(shape = (layers_dims[l],1))
+    return parameters
+
+def initialize_parameters_random(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+    for l in range(1,L):
+        parameters ["W"+str(l)] = np.random.randn(layers_dims[l],layers_dims[l-1])* 10
+        parameters ["b"+str(l)] = np.zeros(shape = (layers_dims[l],1))
+    return parameters
+
+
+
+parameters = model(train_X, train_Y, initialization = "random")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+plt.title("Model with large random initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
---
+++ week 1/part 3 - gradient descent/gradientChecking.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 23:48:21 2019
+
+@author: Nahir
+"""
+
