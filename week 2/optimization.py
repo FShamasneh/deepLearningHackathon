e0dd061baae4f1d2927fa70b35b55508076d74c4
fshamasna <firas.m.shamasneh@gmail.com>
2019-06-13 01:28:07 +0200

    Momentum Optimizer

--- week 2/optimization.py
+++ week 2/optimization.py
@@ -18,6 +18,44 @@ plt.rcParams['image.cmap'] = 'gray'
 def update_parameters_with_gd(parameters, grads, learning_rate):
     L = len(parameters) // 2
     for l in range(L):
-        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate*grads['dW' + str(l+1)]
-        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate*grads['db' + str(l+1)]
+        parameters["W"+str(l+1)] = parameters["W"+str(l+1)] -  learning_rate * grads["dW"+str(l+1)]
+        parameters["b"+str(l+1)] = parameters["b"+str(l+1)] -  learning_rate * grads["db"+str(l+1)]
     return parameters
+
+def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+    np.random.seed(seed)
+    m = X.shape[1]
+    mini_batches = []
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((1,m))
+    num_complete_minibatches = math.floor(m/mini_batch_size)
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k*mini_batch_size : (k+1)*mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k*mini_batch_size : (k+1)*mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, mini_batch_size*num_complete_minibatches:]
+        mini_batch_Y = shuffled_Y[:, mini_batch_size*num_complete_minibatches:]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    return mini_batches
+
+def initialize_velocity(parameters):
+    L = len(parameters) // 2
+    v = {}
+    for l in range(L):
+        v["dW" + str(l+1)] = np.zeros(parameters['W'+str(l+1)].shape)
+        v["db" + str(l+1)] = np.zeros(parameters['b'+str(l+1)].shape)
+    return v 
+
+def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
+    L = len(parameters) // 2
+    for l in range(L):
+        v["dW" + str(l+1)] = beta * v['dW' + str(l+1)] + (1-beta)*(grads['dW' + str(l+1)])
+        v["db" + str(l+1)] = beta * v['db' + str(l+1)] + (1-beta)*(grads['db' + str(l+1)])
+        parameters["W" + str(l+1)] = parameters['W' + str(l+1)] - learning_rate * v['dW' + str(l+1)]
+        parameters["b" + str(l+1)] = parameters['b' + str(l+1)] - learning_rate * v['db' + str(l+1)]
+    return parameters, v
+
