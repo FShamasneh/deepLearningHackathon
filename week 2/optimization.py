0052f08152663df5994e1f45fb959d3b2807100e
fshamasna <firas.m.shamasneh@gmail.com>
2019-06-13 00:01:38 +0200

    optimization methods

---
+++ week 2/optimization.py
@@ -0,0 +1,23 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import math
+import sklearn
+import sklearn.datasets
+
+from opt_utils import load_params_and_grads, initialize_parameters, forward_propagation, backward_propagation
+from opt_utils import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
+from testCases import *
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0) 
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+    for l in range(L):
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate*grads['dW' + str(l+1)]
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate*grads['db' + str(l+1)]
+    return parameters
